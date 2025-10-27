b8897a2633b396a8e0a46584fc18f4e75a79dc8b
vatsalyanallabothula <nallabothulavatsalya70@gmail.com>
2025-10-27 17:07:20 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,21 @@
+import streamlit as st
+st.header("A.P.J. Abdul Kalam")
+st.subheader("The Missile man of india")
+st.write(''' 
+**Dr. A. P. J. Abdul Kalam (1931–2015)** was an Indian aerospace scientist and the **11th President of India**, widely known as the ***"Missile Man"*** for his role in developing India’s missile and space programs. 
+
+Born in **Rameswaram** to a modest family, he rose to become a key figure in the **SLV-III, Agni and Prithvi missile projects**, and contributed to the **1998 Pokhran nuclear tests**. 
+
+A beloved ***“People’s President” (2002–2007)***, he inspired millions of students through his books like *Wings of Fire* and his constant encouragement of innovation, education, and national development. 
+
+He passed away while delivering a lecture to students, reflecting his lifelong dedication to youth and learning.
+''')
+
+st.divider()
+st.markdown("### 🔗 Major Achievements")
+st.write(
+    "[IGMDP](https://en.wikipedia.org/wiki/Integrated_Guided_Missile_Development_Programme)  |  "
+    "[Pokhran-II](https://en.wikipedia.org/wiki/Pokhran-II)  |  "
+    "[Wings of Fire](https://en.wikipedia.org/wiki/Wings_of_Fire_(autobiography))  |  "
+    "[Bharat Ratna](https://en.wikipedia.org/wiki/Bharat_Ratna)"
+)
